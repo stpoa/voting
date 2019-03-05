e68c04f75fdb16b0147d53a9f81770d7a79fea5a
stpoa <stpoaa+dev@gmail.com>
2019-03-05 07:38:24 +0100

    Update contracts

--- client/src/lib/voting.js
+++ client/src/lib/voting.js
@@ -30,17 +30,3 @@ export const vote = ({ contract, gas, from }) => async ({
     .vote(ballotId + '', proposalNumber + '')
     .send({ from, gas })
 }
-
-// const canVote = await contract.methods.canVote(ballotId + '', from).call()
-// if (canVote) {
-// } else {
-// console.log('Cannot vote')
-// }
-
-// const ballot = await contract.methods.ballots(ballotId + '').call()
-
-// const proposalVoteCount = await contract.methods
-//   .proposalVoteCount(ballotId + '', '0')
-//   .call()
-
-// const ballotAfter = await contract.methods.ballots('0').call()
